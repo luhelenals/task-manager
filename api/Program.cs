a5ff533469935c5fa5fea0d5b2704e4779962733
Luiza <luizahelena.lsantos@gmail.com>
2025-04-05 09:04:45 -0300

    configs iniciais

---
+++ api/Program.cs
@@ -0,0 +1,29 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers().AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.Preserve;
+});
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+// Enable the CORS policy
+app.UseCors("AllowFrontend");
+
+app.UseHttpsRedirection();
+
+app.MapControllers();
+
+app.Run();
